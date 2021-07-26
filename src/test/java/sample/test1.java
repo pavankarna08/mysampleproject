865d9c71e5d8ac743e9d316725ff8a4e7b27474d
Pavan Kumar <Pavan Kumar@LAPTOP-OL5JOLO1>
2021-07-26 18:05:41 +0530

    it is my first commit

---
+++ src/test/java/sample/test1.java
@@ -0,0 +1,16 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class test1 
+{
+	@Test
+	public void maethod()
+	{
+		int a=12;
+		int b=15;
+		int c=a+b;
+		System.out.println("Sum of two nymbers is"+c);
+	}
+
+}
